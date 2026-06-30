3f95434312932cc25e0e7e43275b4d2bc666fa0c
Matheus Faustino <matheus.sfp97@gmail.com>
2026-06-30 22:14:55 +0200

    optimize images

--- ArgentBank-Frontend-main/src/components/Header/index.jsx
+++ ArgentBank-Frontend-main/src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom'
-import logo from '../../assets/argentBankLogo.png'
+import logo from '../../assets/argentBankLogo.webp'
 import './header.css'
 
 const Header = () => {
