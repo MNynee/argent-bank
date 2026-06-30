3f104ba4da19d81cb6e10962310b113f9367df77
Matheus Faustino <matheus.sfp97@gmail.com>
2026-06-30 14:06:25 +0200

    first commit

---
+++ ArgentBank-Frontend-main/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowseRouter as Router, Routes, Route } from 'react-router-dom'
+import './main.css'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+      {/* <ScrollToTop /> */}
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/about' element={<About />} /> */}
+          {/* <Route path='/404' element={<Error />} /> */}
+          {/* <Route path='*' element={<Error />} /> */}
+        </Route>
+      </Routes>
+    </Router>
+  </StrictMode>,
+)
